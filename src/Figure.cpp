8e2bc7502d2b75362e2079c8a0671e984eaa9998
IrinaByck <ira002385@gmail.com>
2025-12-14 19:47:15 +0300

    Create Figure.cpp

---
+++ src/Figure.cpp
@@ -0,0 +1,10 @@
+#include "Figure.h"
+
+std::ostream& operator<<(std::ostream& os, const Figure& figure) {
+    figure.print(os);
+    return os;
+}
+std::istream& operator>>(std::istream& is, Figure& figure) {
+    figure.read(is);
+    return is;
+}
